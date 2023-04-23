4598fce2c3a45dc3fbb0439178800b1f81f26f92
victor-oliveira-dev98 <107275644+victor-oliveira-dev98@users.noreply.github.com>
2023-04-23 12:19:49 +0100

    New commit

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ possibleChoises.forEach(possibleChoise => possibleChoise.addEventListener("click
 }))
 
     function generateComputerChoise() {
-        const randomNumber = Math.floor(Math.random() * possibleChoises.length) + 1
-        
+        const randomNumber = Math.floor(Math.random() * possibleChoises.length) + 1 
         if (randomNumber === 1) {
             computerChoise = 'rock'
         }
@@ -26,36 +25,28 @@ possibleChoises.forEach(possibleChoise => possibleChoise.addEventListener("click
         if (randomNumber === 3) {
             computerChoise = 'paper'
         }
-
         computerChoiseDisplay.innerHTML = computerChoise
-
     }
 
     function getResult() {
         if (computerChoise === userChoise) {
             result = 'its a draw!!'
         }
-
         if (computerChoise === 'rock' && userChoise === 'paper') {
             result = 'You Win!!'
         }
-
         if (computerChoise === 'rock' && userChoise === 'scissors') {
             result = 'You Lose!!'
         }
-
         if (computerChoise === 'paper' && userChoise === 'rock') {
             result = 'You Lose!!'
         }
-
         if (computerChoise === 'paper' && userChoise === 'rock') {
             result = 'You Lose!!'
         }
-
         if (computerChoise === 'paper' && userChoise === 'scissors') {
             result = 'You Win!!'
         }
-
         if (computerChoise === 'scissors' && userChoise === 'rock') {
             result = 'You Win!!'
         }
